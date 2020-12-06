3e88bc6c83bfb184df9e061550b12cef4a78762b
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-07 01:19:33 +0200

    lesson-1: added additional task

---
+++ Lesson1/AdditionalTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = GetNumbersRaisedToPow((x) => x % 2 != 0, numbers);
+
+            foreach (var num in result)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+        public static IEnumerable<double> GetNumbersRaisedToPow(Func<int, bool> action, params int[] numbers)
+        {
+            foreach (var number in numbers)
+            {
+                if (action(number))
+                {
+                    yield return Math.Pow(number, 2);
+                }
+            }
+        }
+    }
+}
