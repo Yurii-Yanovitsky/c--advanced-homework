6655765ecd21b16861db9ba54113f3f2011c3f41
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-08 16:37:58 +0200

    lesson-2: added task4

---
+++ Lesson2/Task4/User.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace Task4
+{
+    class User : IEqualityComparer
+    {
+        public string Name { get; }
+        public string LastName { get; }
+        public int ID { get; }
+
+        public User(string name, string lastName, int id)
+        {
+            Name = name;
+            LastName = lastName;
+            ID = id;
+        }
+        public User()
+        {
+
+        }
+
+        public new bool Equals(object x, object y)
+        {
+            if (x is User a && y is User b)
+            {
+                return a.ID == b.ID;
+            }
+            return false;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            if (obj is User a)
+            {
+                return a.ID;
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"ID{ID}";
+        }
+    }
+}
