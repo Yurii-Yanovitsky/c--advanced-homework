775c23f1d1f01409ab195ffdf448f6e7e3448be1
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-13 18:02:50 +0200

    lesson-3: added task3

---
+++ Lesson3/Task3/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+
+namespace Task3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EventHandler _search;
+        private EventHandler _show;
+        private EventHandler _compress;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrivesList_Initialized();
+            new Presenter(this);
+        }
+
+        public event EventHandler Search
+        {
+            add
+            {
+                _search += value;
+            }
+            remove
+            {
+                _search -= value;
+            }
+        }
+        public event EventHandler ShowContent
+        {
+            add
+            {
+                _show += value;
+            }
+            remove
+            {
+                _show -= value;
+            }
+        }
+        public event EventHandler Compress
+        {
+            add
+            {
+                _compress += value;
+            }
+            remove
+            {
+                _compress -= value;
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            _search.Invoke(sender, e);
+        }
+
+        private void ShowContentButton_Click(object sender, RoutedEventArgs e)
+        {
+            _show.Invoke(sender, e);
+        }
+
+        private void CompressButton_Click(object sender, RoutedEventArgs e)
+        {
+            _compress.Invoke(sender, e);
+        }
+
+        private void DrivesList_Initialized()
+        {
+            var drives = DriveInfo.GetDrives();
+
+            foreach (var item in drives)
+            {
+                if (item.DriveType != DriveType.CDRom)
+                {
+                    drivesList.Items.Add(item.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Lesson3/Task3/Model.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Task3
+{
+    class Model
+    {
+        public string FilePath { get; private set; }
+        public bool IsPath { get; private set; }
+
+        public string CompressFile()
+        {
+            FileStream source = new FileStream(FilePath, FileMode.Open);
+            FileStream destination = File.Create(Path.GetDirectoryName(FilePath) + @"\archive.zip");
+
+            GZipStream compressor = new GZipStream(destination, CompressionMode.Compress);
+
+            int inputByte = source.ReadByte();
+            while (inputByte != -1)
+            {
+                compressor.WriteByte((byte)inputByte);
+                inputByte = source.ReadByte();
+
+            }
+
+            compressor.Close();
+
+            return destination.Name;
+        }
+
+        public string ShowFileContent()
+        {
+            StreamReader streamReader = File.OpenText(FilePath);
+            string content = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return content;
+        }
+
+        public bool SearchFile(string targetDirectory, string fileName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(targetDirectory);
+            FileInfo[] fileInfo;
+
+            if (!dir.Exists)
+            {
+                return false;
+            }
+
+            fileInfo = dir.GetFiles(fileName);
+
+            if (fileInfo.Length == 0)
+            {
+                foreach (var directory in dir.GetDirectories())
+                {
+                    if (directory.Attributes == (FileAttributes.System | FileAttributes.Hidden | FileAttributes.Directory))
+                    {
+                        continue;
+                    }
+                    else if (SearchFile(directory.FullName, fileName))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                FilePath = fileInfo[0].FullName;
+                IsPath = true;
+
+                return true;
+            }
+        }
+    }
+}
