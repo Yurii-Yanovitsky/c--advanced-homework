0f54aac4f0acd07a8803b1534219087dd8e2f1e5
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-14 01:35:50 +0200

    lesson-3: added additional task

---
+++ Lesson3/AdditonalTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace AdditonalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.CreateDirectory($@"F:\Folder_{i}");
+            }
+
+            Console.WriteLine("100 folders have just been created! Press any key to delete them: ");
+            Console.ReadLine();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.Delete($@"F:\Folder_{i}");
+            }
+
+            Console.WriteLine("100 foledrs have just been deleted. Press any key to finish");
+            Console.ReadLine();
+        }
+    }
+}
