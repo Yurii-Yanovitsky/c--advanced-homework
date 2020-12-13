8f65d0751137a0ca65bafb28756a9d77d534c71e
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-14 01:36:31 +0200

    lesson-3: added MyDecorators

---
+++ Lesson3/MyDecorators/RepeatWriter.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.IO;
+
+namespace MyDecorators
+{
+    public class RepeatWriter : Writer
+    {
+        private readonly int _count;
+
+        public RepeatWriter(StreamWriter stream, int count)
+        {
+            StreamWriter = stream;
+            _count = count;
+        }
+
+        public RepeatWriter(Writer writer, int count) : this(writer.StreamWriter, count)
+        {
+            PreviousWriter = writer;
+        }
+
+        public override void Write(string text)
+        {
+            StringBuilder newText = new StringBuilder();
+
+            for (int i = 0; i < _count; i++)
+            {
+                newText.Append(text);
+            }
+
+            if (PreviousWriter == null)
+            {
+                StreamWriter.WriteLine(newText.ToString());
+            }
+
+            else if (PreviousWriter != null)
+            {
+                PreviousWriter.Write(newText.ToString());
+            }
+        }
+    }
+}
---
+++ Lesson3/MyDecorators/Writer.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace MyDecorators
+{
+    public abstract class Writer
+    {
+        public StreamWriter StreamWriter { get; protected set; }
+        protected Writer PreviousWriter { get; set; }
+        abstract public void Write(string text);
+        public void Close()
+        {
+            StreamWriter.Close();
+        }
+    }
+}
