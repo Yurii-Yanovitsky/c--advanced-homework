4f7bd04e91475674a4989129abe6157adaad3b99
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-19 02:17:23 +0200

    lesson-4: added additional task

---
+++ Lesson4/AdditionalTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write("Login: ");
+                string login = Console.ReadLine();
+
+                Console.Write("Passwor: ");
+                string password = Console.ReadLine();
+
+                var m1 = Regex.Match(login, @"^[A-z]+$");
+                var m2 = Regex.Match(password, @"^[\S]{5,}$");
+
+                if (!m1.Success || !m2.Success)
+                {
+                    Console.WriteLine("ERROR: invalid login or password!");
+                    Console.WriteLine("Try again");
+                }
+                else
+                {
+                    isValid = true;
+                }
+            }
+
+            Console.WriteLine("It went well!");
+
+            Console.ReadLine();
+        }
+    }
+}
