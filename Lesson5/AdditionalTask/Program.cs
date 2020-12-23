c08d0f5795a56b21086babafb3d161dc89c4eb92
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-23 15:30:41 +0200

    lesson-5: added additional task

---
+++ Lesson5/AdditionalTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDocument = new XDocument(new XElement("MyContacts",
+                     new XElement("Contact",
+                     new XAttribute("TelephoneNumber", "0952232425"), "Vlad")));
+            xDocument.Save("TelephoneBook.xml");
+
+            XmlTextReader xmlReader = new XmlTextReader(File.OpenRead("TelephoneNumber.xml"));
+
+            while (xmlReader.Read())
+            {
+                Console.WriteLine($"NodeType: {xmlReader.NodeType} Name: { xmlReader.Name} Value: {xmlReader.Value}");
+            }
+
+            xmlReader.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
