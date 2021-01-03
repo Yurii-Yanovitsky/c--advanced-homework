720c4d5896bde0380952c01f062a7dc66420f2eb
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-01-03 18:42:44 +0200

    lesson-5: added task3

---
+++ Lesson5/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("TelephoneBook.xml");
+
+            var root = xmlDocument.DocumentElement;
+
+            foreach (XmlElement xElement in root)
+            {
+                if (xElement.HasAttributes && xElement.Name == "Contact")
+                {
+                    Console.WriteLine($"Phone number: {xElement.GetAttribute("TelephoneNumber")}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
