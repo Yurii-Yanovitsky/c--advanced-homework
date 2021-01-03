b90af38c3009719b55273e1df8cbbd42da515a65
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-01-03 23:10:15 +0200

    lesson-6: added additional task

---
+++ Lesson6/AdditionalTask/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdditionalTask
+{
+    public class MyClass<T> : IInterface1, IInterface2
+    {
+        // Поля.         
+        public T myint;
+        private string mystring = "Hello";
+
+        // Конструкторы.
+        public MyClass() { }
+        public MyClass(T i)
+        {
+            this.myint = i;
+        }
+
+        // Свойства.
+        public T myProp
+        {
+            get { return myint; }
+            set { myint = value; }
+        }
+
+        public string MyString
+        {
+            get { return mystring; }
+        }
+
+        // Методы.
+        public void MethodA() { }
+        public void MethodB() { }
+
+        private void MethodC(string str, string str2)
+        {
+            Console.WriteLine(str + str2);
+        }
+
+        public void myMethod(int p1, string p2) { }
+    }
+}
---
+++ Lesson6/AdditionalTask/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Reflection;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(MyClass<int>);
+
+            SomeClassInfo(type);
+            ListMethods(type);
+            ListFields(type);
+            ListProperties(type);
+            ListConstructors(type);
+            ListInterfaces(type);
+            ListGenericArguments(type);
+
+            Console.ReadLine();
+        }
+        static void SomeClassInfo(Type type)
+        {
+            Console.WriteLine(new string('-', 15));
+            Console.WriteLine("MyClass info");
+            Console.WriteLine(new string('-', 15));
+
+            Console.WriteLine($"Name: {type.FullName}");
+            Console.WriteLine($"Base type: {type.BaseType}");
+            Console.WriteLine($"Is it a class?: {type.IsClass}");
+            Console.WriteLine($"Is it an abstract class?: {type.IsAbstract}");
+            Console.WriteLine($"Is it a sealed class?: {type.IsSealed}");
+            Console.WriteLine($"Is it an interface {type.IsInterface}");
+        }
+        static void ListMethods(Type type)
+        {
+            MethodInfo[] methods = type.GetMethods();
+
+            Console.WriteLine(new string('-', 17));
+            Console.WriteLine("MyClass methods:");
+            Console.WriteLine(new string('-', 17));
+
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+
+        static void ListProperties(Type type)
+        {
+            PropertyInfo[] properties = type.GetProperties();
+
+            Console.WriteLine(new string('-', 17));
+            Console.WriteLine("MyClass properties:");
+            Console.WriteLine(new string('-', 17));
+
+            foreach (var prop in properties)
+            {
+                Console.WriteLine(prop.Name);
+            }
+        }
+
+        static void ListFields(Type type)
+        {
+            FieldInfo[] fields = type.GetFields();
+
+            Console.WriteLine(new string('-', 17));
+            Console.WriteLine("MyClass fields:");
+            Console.WriteLine(new string('-', 17));
+
+            foreach (var field in fields)
+            {
+                Console.WriteLine(field.Name);
+            }
+        }
+
+        static void ListConstructors(Type type)
+        {
+            ConstructorInfo[] constructors = type.GetConstructors();
+
+            Console.WriteLine(new string('-', 17));
+            Console.WriteLine("MyClass constructors:");
+            Console.WriteLine(new string('-', 17));
+
+            foreach (var ctor in constructors)
+            {
+                Console.WriteLine(ctor.Name);
+            }
+        }
+
+        static void ListInterfaces(Type type)
+        {
+            Type[] interfaces = type.GetInterfaces();
+
+            Console.WriteLine(new string('-', 17));
+            Console.WriteLine("MyClass interfaces:");
+            Console.WriteLine(new string('-', 17));
+
+            foreach (var inter in interfaces)
+            {
+                Console.WriteLine(inter.Name);
+            }
+        }
+
+        static void ListGenericArguments(Type type)
+        {
+            Type[] genericArgs = type.GetGenericArguments();
+
+            Console.WriteLine(new string('-', 17));
+            Console.WriteLine("MyClass generic arguments:");
+            Console.WriteLine(new string('-', 17));
+
+            foreach (var arg in genericArgs)
+            {
+                Console.WriteLine(arg.Name);
+            }
+        }
+    }
+}
+
