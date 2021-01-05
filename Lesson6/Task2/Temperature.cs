de72a7e91ee841817b694980e4607a49417b2672
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-01-05 23:07:51 +0200

    lesson-6: added task2

---
+++ Lesson6/Task2/Temperature.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2
+{
+    public class Temperature
+    {
+        public decimal Fahrenheit(decimal temperature)
+        {
+            return temperature * 9 / 5 + 32;
+        }
+
+        public decimal Kelvin(decimal temperature)
+        {
+            return temperature + 273.15m;
+        }
+    }
+}
