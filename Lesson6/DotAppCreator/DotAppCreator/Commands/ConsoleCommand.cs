9a25dd444329145da0f3fa576593379265deb975
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-01-05 23:09:19 +0200

    lesson-6: added DotAppCreator

---
+++ Lesson6/DotAppCreator/DotAppCreator/Commands/ConsoleCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+using System.IO;
+using CommandInterface;
+
+namespace DotAppCreator
+{
+    public class ConsoleCommand : ICommand
+    {
+        private XmlDocument _xDoc = new XmlDocument();
+        private string _initialCode;
+
+        public ConsoleCommand()
+        {
+            _xDoc.Load("ConsoleTemplate.xml");
+
+            using (StreamReader reader = new StreamReader("InitialCode.txt"))
+            {
+                _initialCode = reader.ReadToEnd();
+            }
+        }
+
+        public void Execute(string projectName, string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string fullPath = @$"{path}/{projectName}.csproj";
+            _xDoc.Save(fullPath);
+
+            using (StreamWriter writer = new StreamWriter(@$"{path}/Programm.cs"))
+            {
+                writer.WriteLine(_initialCode.Replace("*Project*", projectName));
+            }
+        }
+    }
+}
---
+++ Lesson6/DotAppCreator/DotAppCreator/DotAppCreator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using CommandInterface;
+
+namespace DotAppCreator
+{
+    class DotAppCreator
+    {
+        Dictionary<string, ICommand> _commands;
+        public DotAppCreator()
+        {
+            LoadCommandsForAssembly(new[] { Assembly.GetExecutingAssembly() });
+        }
+        public DotAppCreator(params Assembly[] commandAssemblies)
+        {
+            LoadCommandsForAssembly(commandAssemblies);
+        }
+
+        private void LoadCommandsForAssembly(Assembly[] assemblies)
+        {
+            var commandTypes = assemblies.SelectMany(a => a.GetTypes())
+                .Where(type => typeof(ICommand).IsAssignableFrom(type))
+                .Where(type => !type.IsAbstract)
+                .Where(type => type.Name.EndsWith("Command"));
+
+            _commands = commandTypes.ToDictionary(type => type.Name.Substring(0, type.Name.Length - "Command".Length).ToLower(), type => (ICommand)Activator.CreateInstance(type));
+        }
+
+        public void ExecuteCommand(string commandText)
+        {
+            var (commandName, projName, path) = SplitCommandText(commandText);
+            if (string.IsNullOrEmpty(commandName)
+                || string.IsNullOrEmpty(projName)
+                || string.IsNullOrEmpty(projName))
+            {
+                return;
+            }
+
+            if (_commands.TryGetValue(commandName, out ICommand command))
+            {
+                if (projName.StartsWith('"') && projName.EndsWith('"'))
+                {
+                    command.Execute(projName.Trim('"'), path);
+                    Console.WriteLine($"Project {projName} has been created!");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Command \"dotcreate {commandName}\" is not supported");
+            }
+        }
+        private (string commandName, string projName, string path) SplitCommandText(string commandText)
+        {
+            if (commandText.StartsWith("dotcreate"))
+            {
+                string[] result = commandText.Substring("dotcreate".Length).Trim().Split(' ');
+
+                return result.Length == 3 ? (result[0], result[1], result[2]) : default;
+            }
+            else
+            {
+
+                return default;
+            }
+        }
+    }
+}
