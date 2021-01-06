4c888c5f5e67a9fd9ad20117a3bbf16b22b4cfc6
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-01-06 22:41:27 +0200

    lesson-7: added task2

---
+++ Lesson7/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            myClass.Method1();
+            //myClass.Method2();
+        }
+    }
+    class MyClass
+    {
+        [Obsolete("This is an old method. Use new instead! ")]
+        public void Method1()
+        {
+
+        }
+
+        [Obsolete("This is an old method. Use new instead!", true)]
+        public void Method2()
+        {
+
+        }
+    }
+}
