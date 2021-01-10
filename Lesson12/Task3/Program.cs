984d640b73d77ebfa1876fea5d84eee5cd93a1d4
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-01-10 03:34:35 +0200

    lesson-12: added task3

---
+++ Lesson12/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Task3
+{
+    class Program
+    {
+        static private Mutex mutex = new Mutex(false, "MyMutex:: CODE");
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(Function).Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Function()
+        {
+            mutex.WaitOne();
+
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} ----> has entered the protected area");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("-");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine($"\nThread {Thread.CurrentThread.ManagedThreadId} <---- has left the protected area");
+
+            mutex.ReleaseMutex();
+        }
+    }
+}
